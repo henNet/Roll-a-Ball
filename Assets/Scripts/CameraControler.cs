3426d2c2dbbe13072dd64f0fa42978a16bff5d67
Neto <dmf.n123@gmail.com>
2015-05-16 00:55:49 -0300

    Criado repositório para guardar o jogo

---
+++ Assets/Scripts/CameraControler.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControler : MonoBehaviour {
+
+	public GameObject player;	/* Referenciar o player */
+	private Vector3 offset;
+
+	void Start()
+	{
+		/*Pega a posiçao inicial da camera */
+		offset = transform.position;
+	}
+
+	void LateUpdate()
+	{
+		transform.position = player.transform.position + offset; 
+	}
+}
---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+/*  */
+public class PlayerControler : MonoBehaviour {
+	public float speed;
+	private int count;
+	public Text countText;
+	public Text winText;
+
+	void Start()
+	{
+		count = 0;
+		countText.text = "Count: " + count.ToString();
+		winText.text = "";
+	}
+
+	/* Chamada para realizar calculos fisicos */
+	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis ("Horizontal"); 
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 moviment = new Vector3(moveHorizontal, 0.0F, moveVertical);
+
+		GetComponent<Rigidbody>().AddForce(moviment * speed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.tag == "PickUp") 
+		{
+			other.gameObject.SetActive(false);
+			count = count + 1;
+			countText.text = "Count: " + count.ToString();
+		}
+
+		if(count == 10)
+			winText.text = "You Win!!";
+	}
+}
